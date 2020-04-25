d2d17ccbf601224dacaba2fd009cf8a5d4014a63
Simon Ngeru <ngeru@cs4africa.com>
2020-04-25 14:56:03 +0300

    CRUD operations working

--- MVC/Controllers/EmployeeController.cs
+++ MVC/Controllers/EmployeeController.cs
@@ -19,5 +19,40 @@ namespace MVC.Controllers
 
             return View(empList);
         }
+        public ActionResult AddOrEdit(int id=0)
+        {
+            if(id==0)
+            {
+                return View(new ViewEmployeeModel());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<ViewEmployeeModel>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(ViewEmployeeModel emp)
+        {
+            if(emp.EmployeeID==0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employees", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employees/"+emp.EmployeeID, emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employees/"+id).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("index");
+        }
     }
 }
--- MVC/Models/ViewEmployeeModel.cs
+++ MVC/Models/ViewEmployeeModel.cs
@@ -12,14 +12,21 @@ namespace MVC.Models
         public int EmployeeID { get; set; }
 
         [Display(Name = "First Name")]
+        [Required (ErrorMessage ="This field is compulsory")]
         public string FirstName { get; set; }
 
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "This field is compulsory")]
         public string LastName { get; set; }
 
         [Display(Name = "Position")]
+        [Required(ErrorMessage = "This field is compulsory")]
         public string Position { get; set; }
+
+        [Required(ErrorMessage = "This field is compulsory")]
         public Nullable<int> Age { get; set; }
+
+        [Required(ErrorMessage = "This field is compulsory")]
         public Nullable<int> Salary { get; set; }
     }
 }
